ac2cd48dc2c9b231f844f6f13051e14b994478b8
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-07-13 15:31:24 -0400

    Added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('todo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/AddOn/i);
+  expect(textElement).toBeInTheDocument();
 });
